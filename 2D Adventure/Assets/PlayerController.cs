d766aeaca3860ab1c44bf629be56dd3123e645b2
Hermann <her.tamilia@stud.uniroma3.it>
2022-10-19 13:07:54 +0200

    implementata animazione tramite blend tree

--- 2D Adventure/Assets/PlayerController.cs
+++ 2D Adventure/Assets/PlayerController.cs
@@ -10,7 +10,7 @@ public class PlayerController : MonoBehaviour
     public float collisionOffset = 0.05f;
 
     Vector2 movementInput;
-    Rigidbody2D rb; 
+    Rigidbody2D rb;
     public ContactFilter2D movementFilter;
     Animator animator;
     List<RaycastHit2D> castCollisions = new List<RaycastHit2D>();
@@ -19,12 +19,21 @@ public class PlayerController : MonoBehaviour
     {
         rb = GetComponent<Rigidbody2D>();
         animator = GetComponent<Animator>();
-        
+
     }
 
-    private void FixedUpdate(){
+    void Update()
+    {
+        animator.SetFloat("Horizontal", movementInput.x);
+        animator.SetFloat("Vertical", movementInput.y);
+        animator.SetFloat("Speed", movementInput.sqrMagnitude);
+    }
+
+    void FixedUpdate()
+    {
         //if movement input is != 0 try to move
-        if(movementInput != Vector2.zero){
+        if (movementInput != Vector2.zero)
+        {
             bool success = TryMove(movementInput);
 
             if (!success)
@@ -36,31 +45,16 @@ public class PlayerController : MonoBehaviour
                     success = TryMove(new Vector2(0, movementInput.y));
                 }
             }
-
-            if (movementInput.y < 0 && movementInput.x == 0)
-                animator.SetBool("isMovingDown", success);
-            if (movementInput.y > 0 && movementInput.x == 0)
-                animator.SetBool("isMovingUp", success);
-            if (movementInput.x < 0 && movementInput.y == 0)
-                animator.SetBool("isMovingLeft", success);
-            if (movementInput.x > 0 && movementInput.y == 0)
-                animator.SetBool("isMovingRight", success);
-        }
-        else
-        {
-            animator.SetBool("isMovingDown", false);
-            animator.SetBool("isMovingUp", false);
-            animator.SetBool("isMovingLeft", false);
-            animator.SetBool("isMovingRight", false);
         }
     }
 
-    private bool TryMove(Vector2 direction){
+    private bool TryMove(Vector2 direction)
+    {
         int count = rb.Cast(
-              direction,
-              movementFilter,
-              castCollisions,
-              moveSpeed * Time.fixedDeltaTime + collisionOffset);
+        direction,
+        movementFilter,
+        castCollisions,
+        moveSpeed * Time.fixedDeltaTime + collisionOffset);
 
         if (count == 0)
         {
@@ -71,10 +65,10 @@ public class PlayerController : MonoBehaviour
         {
             return false;
         }
-
     }
 
-    void OnMove(InputValue movementValue){
+    void OnMove(InputValue movementValue)
+    {
         movementInput = movementValue.Get<Vector2>();
     }
 }
