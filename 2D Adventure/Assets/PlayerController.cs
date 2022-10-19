f27f40c58a9d13cc4eddebc464219944ef444a20
Hermann <her.tamilia@stud.uniroma3.it>
2022-10-19 13:34:35 +0200

    Revert "player idle mantiene l'ultima posizione"
    This reverts commit ed7918879f99e33c7b98cce82ad0c3e52158100b.

--- 2D Adventure/Assets/PlayerController.cs
+++ 2D Adventure/Assets/PlayerController.cs
@@ -24,17 +24,9 @@ public class PlayerController : MonoBehaviour
 
     void Update()
     {
-        float horizontal = Input.GetAxisRaw("Horizontal");
-        float vertical = Input.GetAxisRaw("Vertical");
         animator.SetFloat("Horizontal", movementInput.x);
         animator.SetFloat("Vertical", movementInput.y);
         animator.SetFloat("Speed", movementInput.sqrMagnitude);
-
-        if (horizontal != 0 || vertical != 0)
-        {
-            animator.SetFloat("lastMoveX", horizontal);
-            animator.SetFloat("lastMoveY", vertical);
-        }
     }
 
     void FixedUpdate()
