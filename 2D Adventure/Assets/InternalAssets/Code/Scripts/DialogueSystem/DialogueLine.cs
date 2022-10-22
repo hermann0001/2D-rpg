96fd26f05867ba2ff569da7d465d7d35d654508f
Hermann <her.tamilia@stud.uniroma3.it>
2022-10-22 06:04:18 +0200

    Dialogue System perfezionato e funzionante

--- 2D Adventure/Assets/InternalAssets/Code/Scripts/DialogueSystem/DialogueLine.cs
+++ 2D Adventure/Assets/InternalAssets/Code/Scripts/DialogueSystem/DialogueLine.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,27 +6,86 @@ using UnityEngine.UI;
 
 namespace DialogueSystem
 {
-    public class DialogueLine : DialogueBaseClass
+    public class DialogueLine : MonoBehaviour
     {
-        private Text textHolder;
+        public Text textHolder;
+        public string[] lines;
 
+        private int index;
         [Header("Text Options")]
-        [SerializeField] private string input;
         [SerializeField] private Color textColor;
         [SerializeField] private Font textFont;
 
         [Header("Time Parameters")]
         [SerializeField] private float delay;
 
-        [Header("Audio Parameters")]
+        [Header("Sound")]
         [SerializeField] private AudioClip sound;
+
+        [Header("Sound")]
+        [SerializeField] private Sprite characterSprite;
+        [SerializeField] private Image imageHolder;
 
         private void Awake()
         {
             textHolder = GetComponent<Text>();
-            StartCoroutine(WriteText(input, textHolder, textColor, textFont, delay/*, sound*/));
+
+            imageHolder.sprite = characterSprite;
+            imageHolder.preserveAspect = true;
+        }
+
+        private void Start()
+        {
+            textHolder.text = string.Empty;
+            startDialogue();
+        }
+
+        private void Update()
+        {
+            //if you E it will skip to the next line or if the line is not over yet, it will be shown entirely
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                if (textHolder.text.Equals(lines[index]))
+                    NextLine();
+                else
+                {
+                    StopAllCoroutines();
+                    textHolder.text = lines[index];
+                }
+            }
+        }
+
+        private IEnumerator TypeLine()
+        {
+            //iterates on each letter of string $lines[index] and add them to textHolder.text each $delay seconds
+            foreach (char c in lines[index].ToCharArray())
+            {
+                textHolder.text += c;
+                yield return new WaitForSeconds(delay);
+
+            }
+        }
+
+        private void startDialogue()
+        {
+            index = 0;
+            StartCoroutine(TypeLine());
+        }
+
+        private void NextLine()
+        {
+            if (index < lines.Length - 1)
+            {
+                index++;
+                textHolder.text = string.Empty;
+                StartCoroutine(TypeLine());
+            }
+            else
+            {
+                //deactivates the first gameObject's parents (in this case the dialoguePanel is parent of dialogueText
+                gameObject.transform.parent.gameObject.SetActive(false);
+            }
         }
     }
-
-
 }
+
