bb09f6019fc7577328fabd68a10ecc9d390dea0c
Hermann <her.tamilia@stud.uniroma3.it>
2023-03-14 00:26:41 +0100

    cambiato idea

--- gameAssets/TILEPACKS/CosmicLilac/CosmicLilac_Tiles2.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.9" tiledversion="1.9.2" name="CosmicLilac_Tiles" tilewidth="16" tileheight="16" tilecount="104" columns="13">
- <image source="CosmicLilac_Tiles.png" width="208" height="128"/>
-</tileset>
