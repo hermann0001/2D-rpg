5090abe82dffcc8073e8de4738ec1300bba31c7d
Hermann <her.tamilia@stud.uniroma3.it>
2023-03-15 21:39:29 +0100

    mappa_0 update

--- MAPPA/mappa_0/bedroom16x16.tsx
+++ MAPPA/mappa_0/bedroom16x16.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.9" tiledversion="1.9.2" name="bedroom16x16" tilewidth="16" tileheight="16" tilecount="1712" columns="16">
- <image source="../TILEPACKS/Modern_Interiors_v41.3.3/1_Interiors/16x16/Theme_Sorter/4_Bedroom_16x16.png" width="256" height="1712"/>
+ <image source="../../../../gameAssets/TILEPACKS/Modern_Interiors_v41.3.3/1_Interiors/16x16/Theme_Sorter_Shadowless/4_Bedroom_Shadowless.png" width="256" height="1712"/>
 </tileset>
--- MAPPA/mappa_0/laboratory_floor_tiles.tsx
+++ MAPPA/mappa_0/laboratory_floor_tiles.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.9" tiledversion="1.9.2" name="laboratory_floor_tiles" tilewidth="16" tileheight="16" tilecount="198" columns="11">
- <image source="../TILEPACKS/tiles_laboratory_LandOfPixels/16px/tilesFloor.png" width="176" height="288"/>
+ <image source="../../../../gameAssets/TILEPACKS/tiles_laboratory_LandOfPixels/16px/tilesFloor.png" width="176" height="288"/>
 </tileset>
