bf05369f04aa8654b84f89d6798a8c0dc6b688ac
Hermann <her.tamilia@stud.uniroma3.it>
2023-03-16 16:31:20 +0100

    importata mappa con SuperTiled2Unity

---
+++ 2D Adventure/Assets/SuperTiled2Unity/Examples/Overhead/Import/bedroom16x16.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.2" name="bedroom16x16" tilewidth="16" tileheight="16" tilecount="1712" columns="16">
+ <image source="4_Bedroom_Shadowless.png" width="256" height="1712"/>
+</tileset>
---
+++ 2D Adventure/Assets/SuperTiled2Unity/Examples/Overhead/Import/laboratory_floor_tiles.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.2" name="laboratory_floor_tiles" tilewidth="16" tileheight="16" tilecount="198" columns="11">
+ <image source="tilesFloor.png" width="176" height="288"/>
+</tileset>
