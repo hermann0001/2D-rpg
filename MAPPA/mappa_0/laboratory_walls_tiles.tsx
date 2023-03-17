e54adce19709af3be5bf97f00ec1d6d8f0954ec2
Hermann <her.tamilia@stud.uniroma3.it>
2023-03-17 21:51:04 +0100

    rimossa la cartella MAPPA poiché obsoleta

--- MAPPA/mappa_0/laboratory_walls_tiles.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.9" tiledversion="1.9.2" name="laboratory_walls_tiles" tilewidth="16" tileheight="16" tilecount="192" columns="12">
- <image source="../../../../gameAssets/TILEPACKS/tiles_laboratory_LandOfPixels/16px/tilesWalls.png" width="192" height="256"/>
-</tileset>
--- MAPPA/mappa_0/shadows.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.9" tiledversion="1.9.2" name="shadows" tilewidth="16" tileheight="16" tilecount="80" columns="16">
- <image source="../../../../gameAssets/TILEPACKS/Modern_Interiors_v41.3.3/1_Interiors/16x16/Room_Builder_subfiles/Room_Builder_Floor_Shadows_16x16.png" width="256" height="80"/>
-</tileset>
