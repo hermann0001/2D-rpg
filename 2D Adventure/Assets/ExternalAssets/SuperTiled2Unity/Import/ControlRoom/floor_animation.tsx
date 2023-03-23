5ca91f3670d51b94786029b0585e6ef755e91276
lucapeperoni <luc.peperoni@stud.uniroma3.it>
2023-03-23 21:38:08 +0100

    Modifiche cercando di implementare animazione porte

---
+++ 2D Adventure/Assets/ExternalAssets/SuperTiled2Unity/Import/ControlRoom/floor_animation.tsx
@@ -0,0 +1,24 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.0" name="floor_animation" tilewidth="16" tileheight="16" tilecount="48" columns="16">
+ <image source="spriteSheet_tiledLiquids_16x16.png" width="256" height="48"/>
+ <tile id="32">
+  <animation>
+   <frame tileid="32" duration="100"/>
+   <frame tileid="33" duration="100"/>
+   <frame tileid="34" duration="100"/>
+   <frame tileid="35" duration="100"/>
+   <frame tileid="36" duration="100"/>
+   <frame tileid="37" duration="100"/>
+   <frame tileid="38" duration="100"/>
+   <frame tileid="39" duration="100"/>
+   <frame tileid="40" duration="100"/>
+   <frame tileid="41" duration="100"/>
+   <frame tileid="42" duration="100"/>
+   <frame tileid="43" duration="100"/>
+   <frame tileid="44" duration="100"/>
+   <frame tileid="45" duration="100"/>
+   <frame tileid="46" duration="100"/>
+   <frame tileid="47" duration="100"/>
+  </animation>
+ </tile>
+</tileset>
