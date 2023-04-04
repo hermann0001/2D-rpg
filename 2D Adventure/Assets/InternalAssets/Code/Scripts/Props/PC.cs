bfc8b36e8cc1e162fad7f408abe6210ad91a6909
Hermann <her.tamilia@stud.uniroma3.it>
2023-04-04 20:11:56 +0200

    aggiunta interazione con il pc, da fixare il collider

---
+++ 2D Adventure/Assets/InternalAssets/Code/Scripts/Props/PC.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PC : MonoBehaviour, IInteractable
+{
+    private Animator animator;
+    private bool is_interacted;
+    bool IInteractable.CanInteract()
+    {
+        return !is_interacted;
+    }
+
+    void IInteractable.Interact()
+    {
+        Debug.Log("interacted!");
+        is_interacted = true;
+        animator.SetBool("interacted", true);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator =  GetComponent<Animator>();  
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log(is_interacted);
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        Debug.Log(collision.ToString());
+        //if (collision.CompareTag("Player"))
+        //{
+            is_interacted = false;
+            animator.SetBool("interacted", false);
+        //}
+    }
+}
